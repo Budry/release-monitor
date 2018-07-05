c725c9b8254814ca840e8fea1ed6e8b7a55599a4
Budry <zarubaondra@gmail.com>
2018-07-05 18:51:21 +0200

    Added support for multiple runtimes

--- src/providers/provider.go
+++ src/providers/provider.go
@@ -6,6 +6,7 @@ import (
 	"os/exec"
 	"sort"
 	"strings"
+	"sync"
 
 	"bitbucket.org/budry/release-monitor/src/adapters"
 	"bitbucket.org/budry/release-monitor/src/errors"
@@ -18,46 +19,55 @@ type Provider struct {
 	Adapters adapters.Adapters
 }
 
-func (provider *Provider) Process(monitors []monitors.Monitor) {
-	for _, monitor := range monitors {
-		adapter := provider.Adapters.GetAdapter(monitor.Url)
-		if adapter == nil {
-			panic("Missing adapter for monitor '" + monitor.Name + "'")
+func (provider *Provider) ProcessMonitor(monitor monitors.Monitor, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	adapter := provider.Adapters.GetAdapter(monitor.Url)
+	if adapter == nil {
+		panic("Missing adapter for monitor '" + monitor.Name + "'")
+	}
+	releaseRecords := adapter.GetReleases(&monitor)
+	if len(releaseRecords) > 0 {
+
+		sort.Sort(releases.ReleasesByDate(releaseRecords))
+
+		storedData := *store.GetStore()
+		_, exist := storedData[monitor.Name]
+
+		var newReleases []releases.ReleaseRecord
+		if !exist {
+			storedData[monitor.Name] = releaseRecords[0].Date
+			newReleases = append(newReleases, releaseRecords[0])
 		}
-		releaseRecords := adapter.GetReleases(&monitor)
-		if len(releaseRecords) > 0 {
 
-			sort.Sort(releases.ReleasesByDate(releaseRecords))
-
-			storedData := *store.GetStore()
-			_, exist := storedData[monitor.Name]
-
-			var newReleases []releases.ReleaseRecord
-			if !exist {
-				storedData[monitor.Name] = releaseRecords[0].Date
-				newReleases = append(newReleases, releaseRecords[0])
+		for _, release := range releaseRecords {
+			if storedData[monitor.Name].Before(release.Date) {
+				storedData[monitor.Name] = release.Date
+				newReleases = append(newReleases, release)
 			}
+		}
+		store.UpdateStore(storedData)
 
-			for _, release := range releaseRecords {
-				if storedData[monitor.Name].Before(release.Date) {
-					storedData[monitor.Name] = release.Date
-					newReleases = append(newReleases, release)
-				}
-			}
-			store.UpdateStore(storedData)
+		if len(newReleases) > 0 {
+			for _, command := range monitor.Commands {
+				newReleasesString, jsonErr := json.Marshal(newReleases)
+				errors.HandleError(jsonErr)
 
-			if len(newReleases) > 0 {
-				for _, command := range monitor.Commands {
-					newReleasesString, jsonErr := json.Marshal(newReleases)
-					errors.HandleError(jsonErr)
-
-					command = strings.Replace(command, "%%RELEASE%%", string(newReleasesString), -1)
-					out, err := exec.Command("sh", "-c", command).Output()
-					errors.HandleError(err)
-					fmt.Printf("Command: %s", command)
-					fmt.Printf(" | Result: %s\n", out)
-				}
+				command = strings.Replace(command, "%%RELEASE%%", string(newReleasesString), -1)
+				out, err := exec.Command("sh", "-c", command).Output()
+				errors.HandleError(err)
+				fmt.Printf("Command: %s", command)
+				fmt.Printf(" | Result: %s\n", out)
 			}
 		}
 	}
+}
+
+func (provider *Provider) Process(monitorItems []monitors.Monitor) {
+	wg := &sync.WaitGroup{}
+	for _, monitor := range monitorItems {
+		wg.Add(1)
+		go provider.ProcessMonitor(monitor, wg)
+	}
+	wg.Wait()
 }
